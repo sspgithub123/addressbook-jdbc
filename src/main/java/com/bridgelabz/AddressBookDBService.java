bd3cf2ae6388712e1600b3e983a396ccf51f0664
Shubham <ssp671995@gmail.com>
2022-04-22 20:24:25 +0530

    Added the Welcome message in the program.

---
+++ src/main/java/com/bridgelabz/AddressBookDBService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class AddressBookDBService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the address book jdbc program.");
+    }
+}
