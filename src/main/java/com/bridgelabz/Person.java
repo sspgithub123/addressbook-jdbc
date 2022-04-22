11d2f71df3ed3eb1f49596ac282dcd03d3c31f40
Shubham <ssp671995@gmail.com>
2022-04-22 20:42:09 +0530

    Added to connect and retrieve database.

--- src/main/java/com/bridgelabz/Person.java
+++ src/main/java/com/bridgelabz/Person.java
@@ -1,4 +1,39 @@
 package com.bridgelabz;
 
 public class Person {
+    //variable declaration
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String mobileNumber;
+    private String email;
+
+    //paarmeterised constructor
+    public Person(int id, String firstName, String lastName, String address, String city, String state, String zip,
+                  String mobileNumber, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.mobileNumber = mobileNumber;
+        this.email = email;
+
+    }
+
+    //toString metthod
+    @Override
+    public String toString() {
+        String persondetails;
+        persondetails = "\nID: " + id + "\nFirst Name: " + firstName + "\nLast Name: " + lastName + "\nAddress: "
+                + address + "\nCity: " + city + "\nState: " + state + "\nZip: " + zip + "\nMobile Number: "
+                + mobileNumber + "\nEmail: " + email + "\nEntry Date: " ;
+        return persondetails;
+    }
 }
--- src/test/java/com/bridgelabz/AddressBookServiceTest.java
+++ src/test/java/com/bridgelabz/AddressBookServiceTest.java
@@ -1,4 +1,22 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import java.util.List;
+
 public class AddressBookServiceTest {
+    @Before
+    public void init() {
+        System.out.println("Welcome to AddressBook Management Service");
+    }
+
+    @Test
+    public void givenAddressBookDB_WhenRetrivedShouldMatchPersonCount() {
+        AddressBookService addressBookService = new AddressBookService();
+        List<Person> addressBookData = addressBookService.readAddressBookData(AddressBookService.IOService.DB_IO);
+        Assert.assertEquals(0, addressBookData.size());
+    }
 }
